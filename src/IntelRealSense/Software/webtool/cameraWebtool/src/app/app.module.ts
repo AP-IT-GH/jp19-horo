ffd37fcb6e83f4587417176b3d5419b6d49bfe63
jaro <jaro.helsen@hotmail.be>
2019-11-15 11:29:07 +0100

    small edits for seperate color pickers

--- src/IntelRealSense/Software/webtool/cameraWebtool/src/app/app.module.ts
+++ src/IntelRealSense/Software/webtool/cameraWebtool/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColorpickerComponent } from './colorpicker/colorpicker.component';
 
-import {WebcamModule} from 'ngx-webcam'
+import {WebcamModule} from 'ngx-webcam';
 import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
