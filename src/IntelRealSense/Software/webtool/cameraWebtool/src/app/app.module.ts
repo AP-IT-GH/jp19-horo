716e7a573521689dfab92ea55e53d58f15aeb81c
jaro <jaro.helsen@hotmail.be>
2019-11-15 10:32:46 +0100

    added colorpicker

--- src/IntelRealSense/Software/webtool/cameraWebtool/src/app/app.module.ts
+++ src/IntelRealSense/Software/webtool/cameraWebtool/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ColorpickerComponent } from './colorpicker/colorpicker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorpickerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
