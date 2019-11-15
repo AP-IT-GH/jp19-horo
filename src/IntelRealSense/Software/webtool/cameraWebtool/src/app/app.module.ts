344a8b544d0291a34b768a0cc84e5a7727abca82
jaro <jaro.helsen@hotmail.be>
2019-11-15 10:44:44 +0100

    Revert "style changes"
    This reverts commit 99e0a7db68aa006d45ee24d906a02b9f6777cd49.

--- src/IntelRealSense/Software/webtool/cameraWebtool/src/app/app.module.ts
+++ src/IntelRealSense/Software/webtool/cameraWebtool/src/app/app.module.ts
@@ -4,16 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {WebcamModule} from 'ngx-webcam'
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    WebcamModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
