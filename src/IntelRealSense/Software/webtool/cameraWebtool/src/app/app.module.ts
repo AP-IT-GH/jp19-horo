a44d42e85ecdb1b5ff58240f572b2e5103f59791
michiel <michiel.vissers09@gmail.com>
2019-11-15 10:49:42 +0100

    Revert "Merge branch '84-IntelRealsense_Calibration' of https://github.com/AP-Elektronica-ICT/jp19-jp19-horo into 84-IntelRealsense_Calibration"
    This reverts commit 6d1888278f16bf9aea01228c23167628e80975a3, reversing
    changes made to 99e0a7db68aa006d45ee24d906a02b9f6777cd49.

--- src/IntelRealSense/Software/webtool/cameraWebtool/src/app/app.module.ts
+++ src/IntelRealSense/Software/webtool/cameraWebtool/src/app/app.module.ts
@@ -3,21 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ColorPickerModule } from 'ngx-color-picker';
-import { ColorpickerComponent } from './colorpicker/colorpicker.component';
 
 import {WebcamModule} from 'ngx-webcam'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ColorpickerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WebcamModule,
-    ColorPickerModule
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
